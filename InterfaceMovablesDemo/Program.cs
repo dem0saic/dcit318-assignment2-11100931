d01eff15729fd40f6a796b20767a0ac7a1707beb
dem0saic <vowusu032@st.ug.edu.gh>
2025-08-09 01:58:38 +0000

    Task 3: Interfaces - IMovable with Car and Bicycle implementations and demo

---
+++ InterfaceMovablesDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace InterfaceMovablesDemo
+{
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+}
