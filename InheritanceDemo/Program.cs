e7c37b802349c8f833aab9d8d101b425ee854758
dem0saic <vowusu032@st.ug.edu.gh>
2025-08-09 01:57:01 +0000

    Task 1: Inheritance and Method Overriding - Animal/Dog/Cat with MakeSound() and demo program

---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace InheritanceDemo
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            var genericAnimal = new Animal();
+            var dog = new Dog();
+            var cat = new Cat();
+
+            genericAnimal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+}
